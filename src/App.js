01b8da53ba987ece2fa4c5db686c94006d5044ac
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2022-10-01 13:46:52 -0400

    added phone frame desktop

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import PokemonList from './components/list/PokemonList';
 import './assets/sass/style.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PokemonDetail from './components/detail/PokemonDetail';
+import './assets/sass/_app.scss';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PokemonList />} />
-                <Route path="/:pokemonName" element={<PokemonDetail />} />
-            </Routes>
-        </BrowserRouter>
+        <div className="app">
+            <div className="app__frame">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<PokemonList />} />
+                        <Route path="/:pokemonName" element={<PokemonDetail />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </div>
     );
 }
 
